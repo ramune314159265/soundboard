669f624ec2fdd7eba926bb857d4546baf1fa9916
ramune314159265 <ramune314159265@gmail.com>
2025-02-08 23:30:26 +0900

    メニューボタン追加

--- src/components/Sound.jsx
+++ src/components/Sound.jsx
@@ -1,4 +1,11 @@
-import { Button, HStack, Text } from '@chakra-ui/react'
+import {
+	MenuContent,
+	MenuContextTrigger,
+	MenuItem,
+	MenuRoot,
+} from "@/components/ui/menu"
+import { Button, ButtonGroup, HStack, IconButton, Text } from '@chakra-ui/react'
+import { HiOutlineEllipsisVertical } from 'react-icons/hi2'
 
 export const Sound = ({ data }) => {
 	const clickHandle = () => {
@@ -7,16 +14,29 @@ export const Sound = ({ data }) => {
 		audio.play()
 	}
 	return (
-		<Button
-			size={"xl"}
-			variant={"surface"}
-			height={"3rem"}
-			onClick={clickHandle}
-		>
-			<HStack>
-				<Text >{data.emoji}</Text>
-				<Text>{data.name}</Text>
-			</HStack>
-		</Button>
+		<MenuRoot width="12rem" height={"3rem"}>
+			<MenuContextTrigger>
+				<ButtonGroup attached variant="surface" width="full" height="full">
+					<Button
+						size="xl"
+						width="9rem"
+						height="full"
+						flexGrow={1}
+						onClick={clickHandle}
+					>
+						<HStack maxWidth="full">
+							<Text>{data.emoji}</Text>
+							<Text truncate>{data.name}</Text>
+						</HStack>
+					</Button>
+					<IconButton size="xl" height="full">
+						<HiOutlineEllipsisVertical />
+					</IconButton>
+				</ButtonGroup>
+			</MenuContextTrigger>
+			<MenuContent>
+				<MenuItem value="new-txt">ああああ</MenuItem>
+			</MenuContent>
+		</MenuRoot>
 	)
 }
