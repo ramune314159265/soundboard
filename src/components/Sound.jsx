9063c1cd1648c810cd0df85e27488cd2aa72ff82
ramune314159265 <ramune314159265@gmail.com>
2025-02-10 21:40:21 +0900

    バグるので右クリックなしに

--- src/components/Sound.jsx
+++ src/components/Sound.jsx
@@ -1,10 +1,9 @@
 import {
 	MenuContent,
-	MenuContextTrigger,
 	MenuItem,
-	MenuRoot,
+	MenuRoot
 } from "@/components/ui/menu"
-import { Button, ButtonGroup, HStack, IconButton, Text } from '@chakra-ui/react'
+import { Button, ButtonGroup, Center, HStack, IconButton, MenuTrigger, Text } from '@chakra-ui/react'
 import { HiOutlineEllipsisVertical } from 'react-icons/hi2'
 
 export const Sound = ({ data }) => {
@@ -14,26 +13,28 @@ export const Sound = ({ data }) => {
 		audio.play()
 	}
 	return (
-		<MenuRoot width="12rem" height={"3rem"}>
-			<MenuContextTrigger>
-				<ButtonGroup attached variant="surface" width="full" height="full">
-					<Button
-						size="xl"
-						width="9rem"
-						height="full"
-						flexGrow={1}
-						onClick={clickHandle}
-					>
-						<HStack maxWidth="full">
-							<Text>{data.emoji}</Text>
-							<Text truncate>{data.name}</Text>
-						</HStack>
-					</Button>
-					<IconButton size="xl" height="full">
-						<HiOutlineEllipsisVertical />
-					</IconButton>
-				</ButtonGroup>
-			</MenuContextTrigger>
+		<MenuRoot>
+			<ButtonGroup attached variant="surface" width="12rem" height="3rem">
+				<Button
+					size="xl"
+					width="9rem"
+					height="full"
+					flexGrow={1}
+					onClick={clickHandle}
+				>
+					<HStack maxWidth="full">
+						<Text>{data.emoji}</Text>
+						<Text truncate>{data.name}</Text>
+					</HStack>
+				</Button>
+				<IconButton size="xl" height="full">
+					<MenuTrigger width="full" height="full" >
+						<Center>
+							<HiOutlineEllipsisVertical />
+						</Center>
+					</MenuTrigger>
+				</IconButton>
+			</ButtonGroup>
 			<MenuContent>
 				<MenuItem value="new-txt">ああああ</MenuItem>
 			</MenuContent>
