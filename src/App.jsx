e9a669d8c7a1939fa57526053ba807fd4932f26b
ramune314159265 <ramune314159265@gmail.com>
2025-02-17 22:07:35 +0900

    追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import { Grid, GridItem } from '@chakra-ui/react'
+import { ConfigDialog } from './components/ConfigDialog'
 import { Header } from './components/Header'
 import { SoundList } from './components/SoundList'
 
 function App() {
 	return (
-		<Grid  templateRows={"3rem 1fr"} width={"100%"} height={"100%"}>
-			<GridItem>
-				<Header></Header>
-			</GridItem>
-			<GridItem overflowY={"auto"}>
-				<SoundList></SoundList>
-			</GridItem>
-		</Grid>
+		<>
+			<Grid templateRows={"3rem 1fr"} width={"100%"} height={"100%"}>
+				<GridItem>
+					<Header></Header>
+				</GridItem>
+				<GridItem overflowY={"auto"}>
+					<SoundList></SoundList>
+				</GridItem>
+			</Grid>
+			<ConfigDialog></ConfigDialog>
+		</>
 	)
 }
 
