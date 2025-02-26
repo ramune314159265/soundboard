8857ec4b5a3dd75c886bd2e0351af21679330bac
ramune314159265 <ramune314159265@gmail.com>
2025-02-26 14:22:50 +0900

    react scan追加

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,14 @@
+import { scan } from "react-scan"
+
 import { Provider } from "@/components/ui/provider"
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './style.css'
+
+scan({
+	enabled: true,
+})
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
